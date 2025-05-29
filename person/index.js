a95bd088731ab94cea6a4fbcc575063e63819123
neelmyna <nithincheeta@gmail.com>
2025-05-29 13:18:47 +0530

    example 4 connecting and disconnect mongodb

--- person/index.js
+++ person/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+const mongoURI = 'mongodb://127.0.0.1:27017/nithin_db';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB', err);
 });
 
-app.get('/', (req, res) => {
-  res.send('The Response just came from the Backend Express!');
-});
+// Disconnect from MongoDB
+const disconnectFromMongoDB = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log('Disconnected from MongoDB');
+  } catch (err) {
+    console.error('Error disconnecting from MongoDB', err);
+  }
+};
+
+disconnectFromMongoDB();
+
+app.listen(port, () => {
+    console.log('Server is running on port 3000')
+  });
+
+//  console.log(`Server is running on http://localhost:${port}`);
