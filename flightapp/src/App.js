6ac63b478307681625004822fe54d1d37a3849de
neelmyna <nithincheeta@gmail.com>
2025-05-30 16:23:01 +0530

    react examples added

---
+++ flightapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { // Functional component
+  return (
+    <div className="App">
+        <p> My Frist React App </p>
+    </div>
+  );
+}
+
+export default App;
